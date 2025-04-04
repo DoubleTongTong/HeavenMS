d39c3a4ce7ba50204f613f750c7ef80c04f01d83
tim <timchen021@qq.com>
2025-04-04 20:53:29 +0800

    项目初始配置的一些修改
    本地运行成功，可以进入游戏
    
    Signed-off-by: tim <timchen021@qq.com>

--- posix-compile.sh
+++ posix-compile.sh
@@ -8,4 +8,13 @@ cores=$(echo cores/*)
 cores=${cores// /:}
 
 mkdir -p $dist
-javac -d $dist -cp $cores $(find $src -name "*.java")
+
+# Check if a command line argument '--debug' is passed
+if [[ "$@" == *"--debug"* ]]; then
+    javac_options="-g"  # Enable debug information
+    echo "Debug mode enabled. Compilation will include debug information."
+else
+    javac_options=""    # No debug info
+fi
+
+javac $javac_options -d $dist -cp $cores -encoding UTF-8 $(find $src -name "*.java")
--- posix-launch.sh
+++ posix-launch.sh
@@ -4,4 +4,22 @@ cores=$(echo cores/*)
 cores=${cores// /:}
 cp=.:dist:$cores
 
-java -Xmx2048m -Dwzpath=wz -cp $cp net.server.Server
+# Set a custom port for debugging
+debug_port=1234
+
+# Check if --debug argument is passed
+if [[ "$@" == *"--debug"* ]]; then
+    echo "Debug mode enabled. Starting the server in debug mode on port $debug_port..."
+
+    # Start the Java program in debug mode and wait for jdb to attach
+    java -Dfile.encoding=UTF-8 -Xmx2048m -Dwzpath=wz -cp $cp -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=$debug_port net.server.Server &
+
+    # Prompt the user to run the jdb command externally
+    echo "To start debugging, open a new terminal and run the following command:"
+    echo "  jdb -attach localhost:$debug_port"
+
+else
+    echo "Normal mode. Starting the server without debugging..."
+    # Start the Java program normally (no debugging)
+    java -Dfile.encoding=UTF-8 -Xmx2048m -Dwzpath=wz -cp $cp net.server.Server
+fi
